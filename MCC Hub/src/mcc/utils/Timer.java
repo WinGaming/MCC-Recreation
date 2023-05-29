b3126ec2288dfc4e07b3903c51851df3750b6bde
WinGaming <SiegerSpieler@gmx.de>
2023-05-29 10:39:01 +0200

    JavaDoc for Timer

--- MCC Hub/src/mcc/utils/Timer.java
+++ MCC Hub/src/mcc/utils/Timer.java
@@ -8,8 +8,10 @@ public class Timer {
 		return new Timer(pair.getA(), pair.getB());
 	}
 
+	/** The amount of milisesconds the timer should run for */
 	private long milliseconds;
 	
+	/** The target time as unix timestamp */
 	private long activeTarget;
 	
 	public Timer(TimeUnit timeunit, int amount) {
@@ -20,10 +22,20 @@ public class Timer {
 		this.activeTarget = start + this.milliseconds;
 	}
 	
+	/**
+	 * Returns the amount of miliseconds left.
+	 * @param now The current time in miliseconds
+	 * @return the amount of miliseconds left
+	 */
 	public long remaining(long now) {
 		return activeTarget <= 0 ? 0 : activeTarget - now;
 	}
 	
+	/**
+	 * Builds and returns a String in the format HH:MM:SS or, iff no hours are left MM:SS.
+	 * @param now The current time in miliseconds
+	 * @return a String in the format HH:MM:SS or MM:SS
+	 */
 	public String buildText(long now) {
 		long remain = this.remaining(now) / 1000l;
 		long seconds = remain % 60;
