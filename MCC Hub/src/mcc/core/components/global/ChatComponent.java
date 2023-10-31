5fb4d0238eeae85777074caad56d4357074e3a8d
WinGaming <SiegerSpieler@gmx.de>
2023-10-31 21:26:39 +0100

    Update ChatComponent.java

--- MCC Hub/src/mcc/core/components/global/ChatComponent.java
+++ MCC Hub/src/mcc/core/components/global/ChatComponent.java
@@ -33,11 +33,13 @@ public class ChatComponent implements Component, Listener { // TODO: Remove TEMP
     public void onChat(AsyncPlayerChatEvent chatEvent) {
         chatEvent.setCancelled(true);
 
+        System.out.println("Chat event: " + chatEvent.getMessage());
+
         EventPlayer eventPlayer = connector.getEventPlayer(chatEvent.getPlayer());
         Optional<Team> team = MCCEvent.getInstance().getTeamManager().getTeamOf(eventPlayer);
 
         BaseComponent[] chatMessage = new ComponentBuilder()
-                .append(team.map(Team::getTeamSymbol).orElse('#') + "")
+                .append(team.map(Team::getTeamSymbol).orElse('\uE000') + "")
                 .append(eventPlayer.getDisplayName()).color(ChatColor.of(team.map(Team::getTeamHexColor).orElse("#FA3A7F")))
                 .append(": ").color(ChatColor.WHITE)
                 .append(chatEvent.getMessage())
